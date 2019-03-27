dba9be285a545a893f00b3f77f482fba05e3941c
Calin Cristian <calin.crist@gmail.com>
2019-03-27 08:12:10 +0200

    Initial Commit

---
+++ aws_amplify_integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aws_amplify_integration
+//
+//  Created by Calin Cristian on 27/03/2019.
+//  Copyright © 2019 Calin Cristian Ciubotariu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
